15a6574002256c391a6129262503be212d08819e
timger <yishenggudou@gmail.com>
2012-02-19 18:35:47 +0800

    add webp2p

---
+++ app.js
@@ -0,0 +1,64 @@
+/**
+ * @author timger <yishenggudou@gmail.com>
+ * @timger http://weibo.com/zhanghaibo
+ * twitter @yishenggudou http://twitter.com/yishenggudou
+ * @yishenggudou http://twitter.com/yishenggudou
+ * Copyright (c) 2008-2011 timger - released under MIT License 
+ */
+
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , socket = require('socket.io');
+
+var app = module.exports = express.createServer()
+  , io = socket.listen(app);
+
+// Configuration
+
+// io.set('log level', false);
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.logger('dev'));
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.helpers({
+  title: 'Vidchat powered by WebRTC'
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+io.sockets.on('connection', function(client) {
+  client.on('capture', function(data) {
+    io.sockets.emit('capture', data);
+  });
+  client.on('hello', function(data) {
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
